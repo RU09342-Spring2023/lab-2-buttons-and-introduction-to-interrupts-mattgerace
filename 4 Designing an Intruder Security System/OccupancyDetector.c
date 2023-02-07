59ade9a455f5ba9aeeb46525658ee9a3e8c04761
gerace13 <gerace13@students.rowan.edu>
2023-02-07 11:49:53 -0500

    Tested new design using dowhile loops
    scrapped other design and attempted new design using dowhile loops, got success with this

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -1,18 +1,24 @@
+
 /*
  * OccupancyDetector.c
  *
  *  Created on: Feb 3, 2023
  *      Author: Matthew Gerace
+ *
+ *
+ *
  */
 
+
+
 #include <msp430.h>
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-#define ARMED_STATE 0 //defining states that msp could possibly be in
-#define WARNING_STATE 1
-#define ALERT_STATE 2
 
 
-int main(void){
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
 
 
 
@@ -21,85 +27,73 @@ int main(void){
     P1DIR |= BIT0;
 
     P4DIR &= ~BIT1;             // Configure P4.1 to an Input
+    P2DIR &= ~ BIT3;            // Configure P2.3 to an Input
 
     P4REN |= BIT1;               // Enable Resistor on P4.1
     P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
-
-    P4REN |= BIT1;                          // P4.1 pull-up register enable
-    P4IES &= ~BIT1;                         // P4.1 Low --> High edge
-    P4IE |= BIT1;                           // P4.1 interrupt enabled
+    P2REN |= BIT3;                  // Enable resistor on P2.3
+    P2OUT |= BIT3;                  // Configure resistor on P2.3 to be a pullup
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
+
+#define count() //defining count variable
 
 
-    char Button = !(P4IN & BIT1); //assigning char button to bitmask
-    char state = ARMED_STATE; //assigning designated state and default state to armed
-    int count = 0; //creating and int called count to track seconds
 
 
-    while(1){
-
-    switch(state)
+    while(1)
     {
-        case 0:
-        {
-            if(Button)
+
+      if (!(P4IN & BIT1))            // if 4.1 is pressed
+      {
+            int count = 0;                   // creates int and makes it 0
+
+            do
+            {                           //while count <20 keep incrementing count
+                P6OUT &= ~BIT6;         //set green LED to off
+                P1OUT ^= BIT0;          //toggle red LED ON
+
+                __delay_cycles(1000000);  //delay for 1 second
+                count = count + 1;              //Add to counter after every cylce
+
+                if (P4IN & BIT1)      //if button is let go of
+                   {
+                       count = 0; //reset count
+                       break; //exit if statement
+                   }
+
+            } while (count < 10); //do this while count is less than 10
+
+            //upper while loop^^
+            //lower while loop vv
+
+
+            do                           // do while/ after its been in warning for 10 seconds
             {
-                P6OUT &= ~BIT6; //turn P6.6 off
-                state = 1; //state change when button/movement is detected
-            }
+                P1OUT |= BIT0;          //set red LED to high
 
-            if(!Button)
-            {
-                P1OUT &= !BIT0; //standard to turn off P1.0 LED
-                P6OUT ^= BIT6; //toggles P6.6 LED
-                __delay_cycles(500000); //waits 0.5 seconds
-                P6OUT ^= BIT6; //toggles P6.6 LED off
-                __delay_cycles(2500000); //waits 2.5 seconds
-            }
-
-
-        }
-
-        case 1:
-        {
-            if(Button)
-            {
-                state = 1; //keeps it in state 1
-                P1OUT ^= BIT0;  //toggles P1OUT
-                count = count + 1; //count up
-                if (count == 10) //if count reaches 10
+                    if (!(P2IN & BIT3))  //if P2.3 is high
                     {
-                        state = 2; //state change to alert
+                        count = 0; //reset count
+                        P1OUT &= ~BIT0; //set red LED to low
+                        P6OUT ^= BIT6;                       // Toggle P6.6
+                         __delay_cycles(500000);             // Delay for 0.5 seconds
+                         P6OUT ^= BIT6;     //toggle
+                         __delay_cycles(2500000); //delay for 2.5 seconds
                     }
-                __delay_cycles(500000); //wait 0.5s
-                }
+            }while (count == 10); //once count is == 10 execute the alert state
+      }
 
 
-            if(!Button)
-            {
-                state = 0;  //if movement stops being detected state 0
-            }
-
-
-        }
-        case 2:         //case 0 and 1 work as expected, even count variable works as expected, had trouble transitioning into state 2 and keeping LED on
+        else //acts as default
         {
-            if(Button)
-            {
-                state = 2; //stay in alert
-                P1OUT |= BIT0; //turn P1 on constantly
-                __delay_cycles(500000);
-            }
-            if(!Button) //if button is released go back to armed
-            {
-                state = 0;
-            }
+            P1OUT &= ~BIT0;
+            P6OUT ^= BIT6;          // Toggle P6.6
+            __delay_cycles(500000);             // delay for 0.5seconds
+            P6OUT ^= BIT6; //toggle
+            __delay_cycles(2500000); //delay for 2.5 seconds
         }
-    }
-
-
 
     }
+
 }
