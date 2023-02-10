cba59ae7a8a3c8324362fe17d1132f0088964a48
gerace13 <gerace13@students.rowan.edu>
2023-02-09 20:46:25 -0500

    FinalFINAL FINAL

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -1,33 +1,83 @@
+#include <msp430.h>
+#include <GPIO_Driver.h>
 
-/*
- * OccupancyDetector.c
- *
- *  Created on: Feb 3, 2023
- *      Author: Matthew Gerace
+/**
+ * main.c
  *
  *
- *
+ * author: Matt Gerace
+ * edited: 2/9/2023
  */
 
 
+void delay(int t) //delay module
+{
+    int i; //variable i
+    for (i=0;i<t;i++) //for loop that runs
+    {
+        __delay_cycles(100000); //delays for 0.1 sec
+    }
 
-#include <msp430.h>
+}
+
+
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    gpioinit(); //init func
+
+    int count = 0; //counter for switching states
+
+    while(1) //infinite loop
+    {
+        char detect = gpioRead(4,1); //make detect sensitive to P4.1
+        char rst = gpioRead(2, 3); //make rst sensitive to P2.3
+        if(!detect) //checks detect
+        {
+            if(count<10)
+            {
+            gpioWrite(6, 6, 0); //turns off green led
+            gpioWrite(1, 0, 1); //turns on red
+            delay(5); //delays for 0.5s
+            gpioWrite(1,0,0); //turns off red
+            delay(5); //delays for 0.5s
+            count++; //increments count
+            }
+            if(count>=10) //once count hits 10
+            {
+                gpioWrite(1, 0, 1); //perma locks red led
+            }
+
+        }
+        if(detect) //checks detect
+        {
+            if(count < 10) //only executes while count is under 10 to avoid switching back into this state during permalock
+            {
+            gpioWrite(1, 0, 0); //turns red led off
+            gpioWrite(6, 6, 1); //turns green led on
+            delay(5); //delays for 0.5s
+            gpioWrite(6, 6, 0); //green led off
+            delay(30); //delay for 3s
+            }
+        }
+
+        if(!rst) //checks for rst
+        {
+            count = 0; //resets count variable
+        }
 
 
+    }
 
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+}
 
 
+void gpioinit()
+{
+    gpioInit(4, 1, 0); //init P4.1 to input
+    gpioInit(2, 3, 0); //int P2.3 to input
+    gpioInit(1, 0, 1); //init P1.0 to output
+    gpioInit(6, 6, 1); //init P6.6 to output
 
-
-    P6DIR |= BIT6;              // Configure P6.6 to an Output
-    P1DIR |= BIT0;
-
-    P4DIR &= ~BIT1;             // Configure P4.1 to an Input
-    P2DIR &= ~ BIT3;            // Configure P2.3 to an Input
 
     P4REN |= BIT1;               // Enable Resistor on P4.1
     P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
@@ -35,65 +85,6 @@ int main(void)
     P2OUT |= BIT3;                  // Configure resistor on P2.3 to be a pullup
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-
-#define count() //defining count variable
-
-
-
-
-    while(1)
-    {
-
-      if (!(P4IN & BIT1))            // if 4.1 is pressed
-      {
-            int count = 0;                   // creates int and makes it 0
-
-            do
-            {                           //while count <20 keep incrementing count
-                P6OUT &= ~BIT6;         //set green LED to off
-                P1OUT ^= BIT0;          //toggle red LED ON
-
-                __delay_cycles(1000000);  //delay for 1 second
-                count = count + 1;              //Add to counter after every cylce
-
-                if (P4IN & BIT1)      //if button is let go of
-                   {
-                       count = 0; //reset count
-                       break; //exit if statement
-                   }
-
-            } while (count < 10); //do this while count is less than 10
-
-            //upper while loop^^
-            //lower while loop vv
-
-
-            do                           // do while/ after its been in warning for 10 seconds
-            {
-                P1OUT |= BIT0;          //set red LED to high
-
-                    if (!(P2IN & BIT3))  //if P2.3 is high
-                    {
-                        count = 0; //reset count
-                        P1OUT &= ~BIT0; //set red LED to low
-                        P6OUT ^= BIT6;                       // Toggle P6.6
-                         __delay_cycles(500000);             // Delay for 0.5 seconds
-                         P6OUT ^= BIT6;     //toggle
-                         __delay_cycles(2500000); //delay for 2.5 seconds
-                    }
-            }while (count == 10); //once count is == 10 execute the alert state
-      }
-
-
-        else //acts as default
-        {
-            P1OUT &= ~BIT0;
-            P6OUT ^= BIT6;          // Toggle P6.6
-            __delay_cycles(500000);             // delay for 0.5seconds
-            P6OUT ^= BIT6; //toggle
-            __delay_cycles(2500000); //delay for 2.5 seconds
-        }
-
-    }
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
 }
